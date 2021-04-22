ac1f5a48837840d0d2d1ad8775346c8eeb8c673a
Fabio-Colombo <thesmallworld95@gmail.com>
2021-04-22 12:07:03 +0200

    Navbar Completa Css

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,6 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/main.js', 'js')
    .sass('src/scss/master.scss', 'css')
-   .setPublicPath('dist');
+   .setPublicPath('dist').options({
+       processCssUrls: false
+   });
